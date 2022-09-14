d584936bed9ec8f5aa720f3f09a04c44a2c81683
frestytania <frestytania@gmail.com>
2022-09-14 22:05:40 +0700

    Update Lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Fresty'
+    }
     return render(request, "wishlist.html", context)
 
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Fresty'
-}
+def show_xml(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data_barang_wishlist), 
+                        content_type="application/xml")
+
+def show_json(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data_barang_wishlist), 
+                        content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data_barang_by_id = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data_barang_by_id), 
+                        content_type="application/xml")
+
+def show_json_by_id(request,id):
+    data_barang_by_id = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data_barang_by_id), 
+                        content_type="application/json")
+
